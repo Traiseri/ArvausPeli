0559ec2ebcd85b6f5abe214acad060e612bbb005
Traiseri <Ville2001@gmail.com>
2023-09-07 13:12:54 +0300

    Skanneri

--- src/ArvausPeli.java
+++ src/ArvausPeli.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class ArvausPeli {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		int i = 0;
+		boolean win = false;
+		String guess;
+		
+		while (win==false) {
+				
+			System.out.println("Guess my name (type stop to exit)");
+			guess =in.nextLine();
+			i++;
+				
+				if (guess.equals("Emma")) {
+					win = true;
+					System.out.println("Congratulations!");
+					System.out.println("You guessed " + i + " times.");
+					
+				}
+				else if (guess.equals("stop")){
+					i--;
+					System.out.println("You guessed " + i + " times.");
+					break;
+				}
 
 	}
 
+}
 }
